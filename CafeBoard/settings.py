56f92b9b40df8d6495d278a9d09e25a402bd2a7c
leoniemclaughlin7 <>
2023-09-12 22:04:22 +0000

    Add allauth with templates

--- CafeBoard/settings.py
+++ CafeBoard/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'crispy_forms',
@@ -52,6 +56,10 @@ INSTALLED_APPS = [
 ]
 
 CRISPY_TEMPLLATE_PACK = 'bootstrap4'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -61,6 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'CafeBoard.urls'
--- CafeBoard/urls.py
+++ CafeBoard/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('review.urls'), name='review_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
