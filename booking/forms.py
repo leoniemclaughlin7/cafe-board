34fb8b07ce503a4ef137cb034e5e4e2e78a59458
leoniemclaughlin7 <>
2023-10-02 16:49:13 +0000

    Add forms.py for customer and booking in the booking app

---
+++ booking/forms.py
@@ -0,0 +1,14 @@
+from .models import Customer, Booking
+from django import forms
+
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ('booking_date', 'booking_time', 'number_attending')
