c3bf1e51879874459e664cf67f621ed815edf063
leoniemclaughlin7 <>
2023-10-05 18:03:39 +0000

    Add a date picker to the booking form in the booking app

--- booking/forms.py
+++ booking/forms.py
@@ -1,5 +1,6 @@
 from .models import Customer, Booking
 from django import forms
+from django.forms.widgets import DateInput
 
 
 class CustomerForm(forms.ModelForm):
@@ -7,10 +8,11 @@ class CustomerForm(forms.ModelForm):
         model = Customer
         fields = ('first_name', 'last_name', 'email', 'phone_number')
 
-
+# https://stackoverflow.com/questions/61077802/how-to-use-a-datepicker-in-a-modelform-in-django 
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
         fields = ('booking_date', 'booking_time', 'number_attending')
-
-
+        widgets = {
+            'booking_date': DateInput(attrs={'type': 'date'}),
+        }
