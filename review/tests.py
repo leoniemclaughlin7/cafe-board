fd03d10bfa299698742e4fdbf69d02d246ed867c
leoniemclaughlin7 <>
2023-10-25 18:15:07 +0000

    Add tests for menu.html views in review app

--- review/tests.py
+++ review/tests.py
@@ -24,3 +24,19 @@ class ReviewTestCase(TestCase):
         response = self.client.get(reverse('review'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+
+class MenuTestCase(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/menu')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('menu'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('menu'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'menu.html')
