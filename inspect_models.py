1a0a4a14ee92018be794dd5300f190f6ae3ad129
leoniemclaughlin7 <>
2023-10-27 22:08:37 +0000

    Fix indentation and error in env.py

--- inspect_models.py
+++ inspect_models.py
@@ -2,9 +2,12 @@
 # You'll have to do the following manually to clean this up:
 #   * Rearrange models' order
 #   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the
+# desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create,
+# modify, and delete the table
+# Feel free to rename the models, but don't rename db_table
+# values or field names.
 from django.db import models
 
 
@@ -119,7 +122,8 @@ class DjangoAdminLog(models.Model):
     object_repr = models.CharField(max_length=200)
     action_flag = models.SmallIntegerField()
     change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING,
+                                     blank=True, null=True)
     user = models.ForeignKey(AuthUser, models.DO_NOTHING)
 
     class Meta:
@@ -223,7 +227,8 @@ class SocialaccountSocialtoken(models.Model):
     token_secret = models.TextField()
     expires_at = models.DateTimeField(blank=True, null=True)
     account = models.ForeignKey(SocialaccountSocialaccount, models.DO_NOTHING)
-    app = models.ForeignKey(SocialaccountSocialapp, models.DO_NOTHING, blank=True, null=True)
+    app = models.ForeignKey(SocialaccountSocialapp, models.DO_NOTHING,
+                            blank=True, null=True)
 
     class Meta:
         managed = False
