fdb85f49f0f609f8eb3006a5c109bfba865d5194
leoniemclaughlin7 <>
2023-10-30 22:49:44 +0000

    Add success message when user leaves a review

--- review/views.py
+++ review/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from .forms import ReviewForm
 from django.views import generic, View
 from .models import Review
+from django.contrib import messages
 
 
 def review(request):
     """
-    View to display review and if post method then leave a review
+    View to display review and if post method then leave a review.
     """
     reviews = Review.objects.order_by('-created_on').all()
     if request.method == 'POST':
@@ -16,6 +17,8 @@ def review(request):
             review_form.instance.name = request.user.username
             review_form.save()
             review_form = ReviewForm()
+            messages.add_message(request, messages.SUCCESS,
+                                'Your review has been successfully posted!')
     else:
         review_form = ReviewForm()
 
@@ -25,6 +28,6 @@ def review(request):
 
 def menu(request):
     """
-    View to render the menu.html page
+    View to render the menu.html page.
     """
     return render(request, 'menu.html')
