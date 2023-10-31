e2978187afb7622b9490a8ea563216edf5cc4497
leoniemclaughlin7 <>
2023-10-31 00:06:11 +0000

    Test send_email view in mail app

--- mail/tests.py
+++ mail/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class MailTestCase(TestCase):
+
+    def test_view_url_exists_at_desired_location(self):
+        """
+        Test to determine if the mail url exists
+        """
+        response = self.client.get('/contact/mail')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        """
+        Test to determine if the mail view is accessable by name.
+        """
+        response = self.client.get(reverse('mail'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        """
+        Test to determine if the mail view uses the correct template. 
+        """
+        response = self.client.get(reverse('mail'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'contact.html')
+        
