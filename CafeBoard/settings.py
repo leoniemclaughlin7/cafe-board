1dc3cccdef999faa377a667abf45d95e6917a7c0
leoniemclaughlin7 <>
2023-11-04 21:55:18 +0000

    Add pass statment to static files in settings.py

--- CafeBoard/settings.py
+++ CafeBoard/settings.py
@@ -178,8 +178,9 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 if DEBUG:
-   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+   pass
 else:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
