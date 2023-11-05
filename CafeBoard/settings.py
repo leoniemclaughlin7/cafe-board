6a2b485ee9d1fb96eb135d168f5340ff1aa7f3e7
leoniemclaughlin7 <>
2023-11-05 17:20:14 +0000

    Change conditional statment for static files in settings.py

--- CafeBoard/settings.py
+++ CafeBoard/settings.py
@@ -178,10 +178,10 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
 if DEBUG:
-   pass
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 else:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
